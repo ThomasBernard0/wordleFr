6c20e487332abf873ad6eaef82ce07d6d1031cf9
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2022-01-27 14:24:49 +0100

    clean up

--- components/Grid.js
+++ components/Grid.js
@@ -1,10 +1,6 @@
 import Row from "./Row";
-import { useEffect } from "react";
 
 export default function Grid({ testedWord, answer }) {
-  useEffect(() => {
-    console.log("hello world");
-  }, []);
   return (
     <div className="grid">
       {testedWord.map((word, index) => (
