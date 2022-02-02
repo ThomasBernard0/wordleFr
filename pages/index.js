ab3dcfd2234e639e5b71daba5d6d0b4278995da7
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2022-02-02 17:04:42 +0100

    syles

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,6 @@ export default function Home() {
         setDisplayWin={setDisplayWin}
         setDisplayLoss={setDisplayLoss}
       />
-      <div className="secret">{answer}</div>
       {displayWin && (
         <ModalVictory
           setDisplayWin={setDisplayWin}
