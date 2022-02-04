973be3b049dda7eabc2342355a10e4bbf799b08f
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2022-02-04 17:45:34 +0100

    true logic behind grid colors

--- components/Row.js
+++ components/Row.js
@@ -4,24 +4,50 @@ import { useState, useEffect } from "react";
 export default function Row({ word, answer, isValid, isUsed }) {
   const length = [0, 1, 2, 3, 4];
   const [letterState, setLetterState] = useState([]);
+  const [letterData, setLetterData] = useState([]);
+  useEffect(() => {
+    setLetterData(
+      word.map((letter) => {
+        let letterInAnswerCount = 0;
+        let letterCorrectlyPlacedCount = 0;
+        for (let i = 0; i < 5; i++) {
+          if (letter === answer.split("")[i]) {
+            letterInAnswerCount++;
+            if (word[i] === answer.split("")[i]) {
+              letterCorrectlyPlacedCount++;
+            }
+          }
+        }
+        return { letter, letterInAnswerCount, letterCorrectlyPlacedCount };
+      })
+    );
+  }, [word]);
+
   useEffect(() => {
     setLetterState(
       word.map((letter, index) => {
         if (letter.length === 0) return { color: "neutral", letter: "" };
+        if (letter === answer[index]) return { color: "green", letter: letter };
         let color = "neutral";
+        let newData = letterData;
         if (isUsed === true) {
           color = "dark";
         }
-        if (answer.includes(letter)) {
+        if (
+          letterData[index].letterInAnswerCount >
+          letterData[index].letterCorrectlyPlacedCount
+        ) {
           color = "orange";
-        }
-        if (letter === answer[index]) {
-          color = "green";
+          for (let i = 0; i < 5; i++) {
+            if (letter === newData[i]?.letter) {
+              newData[i].letterCorrectlyPlacedCount++;
+            }
+          }
         }
         return { letter, color };
       })
     );
-  }, [word]);
+  }, [letterData]);
 
   return (
     <div className="row">
